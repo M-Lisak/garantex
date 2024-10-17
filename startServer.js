fe373ec334673cf71b75c406ebfb062193d986ba
nazzar123 <139446554+nazzar123@users.noreply.github.com>
2024-10-17 09:43:44 +0300

    added func

---
+++ startServer.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express()
+const port = 3005
+
+app.get('/rates', async (req, res) => {
+    try {
+        const { value } = req.query
+
+        if(!value) {
+            res.send("вы не ввели value")
+            return
+        }
+        const rates = await getExchangeRate(value)
+
+        res.send(rates)
+    } catch (e) {
+        console.log('Loc error get rates', e)
+        res.send('Loc error get rates')
+    }
+})
+
+app.listen(port, () => console.log(`startServer on port ${port}!`))
